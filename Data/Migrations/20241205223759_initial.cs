fdda58f13f41a9740c6ef380badc3c6e6f75949b
ducxinloi123 <ducanhan800@gmail.com>
2024-12-12 18:54:48 +0700

    project shoppe

---
+++ Data/Migrations/20241205223759_initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shoppee.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "decimal(8,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductDiscount",
+                table: "Products",
+                type: "decimal(2,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,2)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "decimal(8,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductDiscount",
+                table: "Products",
+                type: "decimal(2,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(2,2)",
+                oldNullable: true);
+        }
+    }
+}
